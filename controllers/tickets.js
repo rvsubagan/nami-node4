1e46d6c0bbdc549d03c4cbc51a90bd2641b387e4
Ryan Dave Subagan <rvsubagan@gmail.com>
2022-10-10 09:46:25 +0800

    Added feature model!

--- controllers/tickets.js
+++ controllers/tickets.js
@@ -1,3 +1,13 @@
-export const getTickets = (req, res) => {
-    res.status(200).json({ message: "Get all tickets" })
+import Ticket from '../model/tickets.js';
+
+export const getTickets = async (req, res) => {
+   try {
+    const tickets = await Ticket.find();
+
+    res.status(200).json(tickets);
+    
+   } catch(error) {
+    res.status(404).json({ message: error.message });
+   }
+
 }
